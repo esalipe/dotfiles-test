5780e3c5e028667853784b96bb7be3ebb25c12d1
Peter Salin <esalipe@users.noreply.github.com>
2019-07-09 18:02:22 +0000

    fetch dotmgr from submodule
    Fetches the dotfile manager from its own repo to keep the dotfiles
    and the dotfile manager separated.

--- install.sh
+++ install.sh
@@ -10,4 +10,8 @@ DOTMGR_DIR=".dotmgr"
 DOTMGR_BIN="dotmgr.sh"
 
 cd "${BASEDIR}"
+
+git -C "${DOTMGR_DIR}" submodule sync --quiet --recursive
+git submodule update --init --recursive "${DOTMGR_DIR}"
+
 "${BASEDIR}/${DOTMGR_DIR}/${DOTMGR_BIN}" --conffile "${CONFIG}" "${@}"
